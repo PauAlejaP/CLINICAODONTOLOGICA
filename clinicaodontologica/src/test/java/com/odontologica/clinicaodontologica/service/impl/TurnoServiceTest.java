0d0d0289d93b4338576da104c79c1f99812e57c6
PauAlejaP <pa.al.pa.ag12@gmail.com>
2023-11-30 19:57:58 -0500

    VERSIÓN PARA ENTREGAR. ERRORES -- NO GENERA LISTA EN TURNO

--- clinicaodontologica/src/test/java/com/odontologica/clinicaodontologica/service/impl/TurnoServiceTest.java
+++ clinicaodontologica/src/test/java/com/odontologica/clinicaodontologica/service/impl/TurnoServiceTest.java
@@ -4,7 +4,10 @@ import com.odontologica.clinicaodontologica.dto.entrada.odontologo.OdontologoEnt
 import com.odontologica.clinicaodontologica.dto.entrada.paciente.DomicilioEntradaDto;
 import com.odontologica.clinicaodontologica.dto.entrada.paciente.PacienteEntradaDto;
 import com.odontologica.clinicaodontologica.dto.entrada.turno.TurnoEntradaDto;
+import com.odontologica.clinicaodontologica.dto.salida.odontologo.OdontologoSalidaDto;
+import com.odontologica.clinicaodontologica.dto.salida.paciente.PacienteSalidaDto;
 import com.odontologica.clinicaodontologica.dto.salida.turno.TurnoSalidaDto;
+import com.odontologica.clinicaodontologica.exception.BadRequestException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import com.odontologica.clinicaodontologica.exception.ResourceNotFoundException;
@@ -14,37 +17,95 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-
+import com.odontologica.clinicaodontologica.utils.LocalDateTimeAdapter;
 @SpringBootTest
 class TurnoServiceTest {
 
     @Autowired
     private TurnoService turnoService;
-
-
+    @Autowired PacienteService pacienteService;
+    @Autowired
+    private OdontologoService odontologoService;
     @Test
-    @Order(2)
-    void alIntentarEliminarUnTurnoYaEliminado_deberiaLanzarUnaResourceNotFoundException(){
+    @Order(1)
+    public void registrarTurno_deberiaAgregarTurno() throws BadRequestException {
 
-        try{
-            turnoService.eliminarTurno(1L);
-        } catch (Exception exception){
-            exception.printStackTrace();
-        }
 
-        assertThrows(ResourceNotFoundException.class, () -> turnoService.eliminarTurno(1L));
+        OdontologoEntradaDto odontologo1 = new OdontologoEntradaDto();
+        odontologo1.setMatricula("123");
+        odontologo1.setNombre("John");
+        odontologo1.setApellido("Doe");
+        OdontologoSalidaDto odontologoGuardado = odontologoService.registrarOdontologo(odontologo1);
+
+        PacienteEntradaDto paciente1 = new PacienteEntradaDto();
+        paciente1.setNombre("Alice");
+        paciente1.setApellido("Smith");
+        PacienteSalidaDto pacienteGuardado = pacienteService.registrarPaciente(paciente1);
+
+
+        TurnoEntradaDto turnoEntradaDto = new TurnoEntradaDto();
+        turnoEntradaDto.setFechaYHora(LocalDateTime.parse("2023-12-01 10:00:00"));
+        turnoEntradaDto.setOdontologoId(odontologoGuardado.getId());
+        turnoEntradaDto.setPacienteId(pacienteGuardado.getId());
+
+        TurnoSalidaDto turnoSalidaDto = turnoService.registrarTurno(turnoEntradaDto);
+
+
+        assertNotNull(turnoSalidaDto);
+        assertNotNull(turnoSalidaDto.getId());
+
+
+        List<TurnoSalidaDto> turns = turnoService.listarTurnos();
+        assertTrue(turns.contains(turnoSalidaDto));
     }
 
     @Test
-    @Order(3)
-    void deberiaDevolverUnaListaVaciaDeturno(){
+    public void listarTurnos_deberiaListarTurnos() throws BadRequestException {
 
-        List<TurnoSalidaDto> turnoDto = turnoService.listarTurnos();
 
-        assertTrue(turnoDto.isEmpty());
+        TurnoEntradaDto turno1 = new TurnoEntradaDto();
+        turno1.setFechaYHora(LocalDateTime.parse("2023-12-01 10:00:00"));
+        turno1.setOdontologoId(1L);
+        turno1.setPacienteId(1L);
+        turnoService.registrarTurno(turno1);
 
+        TurnoEntradaDto turno2 = new TurnoEntradaDto();
+        turno2.setFechaYHora(LocalDateTime.parse("2023-12-02 14:30:00"));
+        turno2.setOdontologoId(2L);
+        turno2.setPacienteId(2L);
+        turnoService.registrarTurno(turno2);
+
+
+        List<TurnoSalidaDto> turnos = turnoService.listarTurnos();
+
+        // Assert
+        assertNotNull(turnos);
+        assertEquals(2, turnos.size()); // Assuming two turns were registered
+
+
+        assertEquals("2023-12-01 10:00:00", turnos.get(0).getFechaYHora());
+        assertEquals(1L, turnos.get(0).getOdontologoSalidaDto().getId());
+        assertEquals(1L, turnos.get(0).getPacienteSalidaDto().getId());
     }
 
+    @Test
+    public void buscarTurnoPorId_deberiaEncontrarTurno() throws BadRequestException {
+
+        // Register a turn
+        TurnoEntradaDto turno1 = new TurnoEntradaDto();
+        turno1.setFechaYHora(LocalDateTime.parse("2023-12-01 10:00:00"));
+        turno1.setOdontologoId(1L);
+        turno1.setPacienteId(1L);
+        TurnoSalidaDto turnoGuardado = turnoService.registrarTurno(turno1);
 
 
+        TurnoSalidaDto turnoEncontrado = turnoService.buscarTurnoPorId(turnoGuardado.getId());
+
+
+        assertNotNull(turnoEncontrado);
+        assertEquals(turnoGuardado.getId(), turnoEncontrado.getId());
+        assertEquals(turnoGuardado.getFechaYHora(), turnoEncontrado.getFechaYHora());
+        assertEquals(turnoGuardado.getOdontologoSalidaDto().getId(), turnoEncontrado.getOdontologoSalidaDto().getId());
+        assertEquals(turnoGuardado.getPacienteSalidaDto().getId(), turnoEncontrado.getPacienteSalidaDto().getId());
+    }
 }
