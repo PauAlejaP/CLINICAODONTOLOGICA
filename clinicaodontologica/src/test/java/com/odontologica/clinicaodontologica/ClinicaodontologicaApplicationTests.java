baf4c09128949fc937869b096df97e2f0c124dd5
PauAlejaP <pa.al.pa.ag12@gmail.com>
2023-12-06 15:08:42 -0500

    VERSIÓN PARA ENTREGAR. ERRORES -- NO GENERA LISTA EN TURNO

--- clinicaodontologica/src/test/java/com/odontologica/clinicaodontologica/ClinicaodontologicaApplicationTests.java
+++ clinicaodontologica/src/test/java/com/odontologica/clinicaodontologica/ClinicaodontologicaApplicationTests.java
@@ -3,22 +3,14 @@ package com.odontologica.clinicaodontologica;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> V5-ultimate
 @SpringBootTest
 class ClinicaodontologicaApplicationTests {
 
 	@Test
 	void contextLoads() {
-<<<<<<< HEAD
-	}
 
-=======
 
 	}
 
-
->>>>>>> V5-ultimate
 }
